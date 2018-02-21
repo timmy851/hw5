8a269845e507dd47eff1879fa4a21b74902d3c6b
timmy851 <jmelizanis@gmail.com>
2018-02-20 23:18:43 -0800

    Update smoke-arrays.js

--- smoke-arrays.js
+++ smoke-arrays.js
@@ -1,1 +1,39 @@
+var a=[];
+var b=[];
+var c=[];
+var xspeed=[];
+var yspeed=[];
 
+
+function setup() {
+  createCanvas(600, 600);
+	
+	for(square=0;square<=100;square=square+1){
+		a[square]= random(0,600)
+		b[square]= random(0,600)
+		c[square] = random(1,0.4)
+		xspeed[square] = random (1,2)
+		yspeed[square] = random(0.1,0.1)
+  }
+}
+  
+function draw() {
+  background(255,105,180);
+  noStroke();
+
+	for(square=0;square<=100;square=square+1){
+		
+		push()
+		fill(b[square]);
+		translate(a[square],b[square])
+		rotate(c[square])
+		rect(5, 10, 40, 50);
+		b[square]=b[square]-xspeed[square]
+		c[square] = c[square] + yspeed[square]
+		pop()
+		
+			if (b[square] < -100) {
+				b[square]= random(40,600);
+			}
+	}
+}
